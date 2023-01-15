1a1a2729bf2220cf0eda749c920e7e2e2f10cc84
aoha <aoha.kurobane168@gmail.com>
2023-01-15 18:29:34 +0900

    ポケモンのタイプ表示を追加

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,25 +3,27 @@ DROP TABLE IF EXISTS pokemon;
 CREATE TABLE pokemon (
  id int unsigned AUTO_INCREMENT,
  name VARCHAR(20) NOT NULL,
+ type1 VARCHAR(20) NOT NULL,
+ type2 VARCHAR(20),
  PRIMARY KEY(id)
 );
 
-INSERT INTO pokemon (id, name) VALUES (1, "フシギダネ");
-INSERT INTO pokemon (id, name) VALUES (2, "フシギソウ");
-INSERT INTO pokemon (id, name) VALUES (3, "フシギバナ");
-INSERT INTO pokemon (id, name) VALUES (4, "ヒトカゲ");
-INSERT INTO pokemon (id, name) VALUES (5, "リザード");
-INSERT INTO pokemon (id, name) VALUES (6, "リザードン");
-INSERT INTO pokemon (id, name) VALUES (7, "ゼニガメ");
-INSERT INTO pokemon (id, name) VALUES (8, "カメール");
-INSERT INTO pokemon (id, name) VALUES (9, "カメックス");
-INSERT INTO pokemon (id, name) VALUES (10, "キャタピー");
-INSERT INTO pokemon (id, name) VALUES (11, "トランセル");
-INSERT INTO pokemon (id, name) VALUES (12, "バタフリー");
-INSERT INTO pokemon (id, name) VALUES (13, "ビードル");
-INSERT INTO pokemon (id, name) VALUES (14, "コクーン");
-INSERT INTO pokemon (id, name) VALUES (15, "スピアー");
-INSERT INTO pokemon (id, name) VALUES (16, "ポッポ");
-INSERT INTO pokemon (id, name) VALUES (17, "ピジョン");
-INSERT INTO pokemon (id, name) VALUES (18, "ピジョット");
-INSERT INTO pokemon (id, name) VALUES (19, "コラッタ");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (1, "フシギダネ", "くさ", "どく");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (2, "フシギソウ", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (3, "フシギバナ", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (4, "ヒトカゲ", "ほのお", "");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (5, "リザード", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (6, "リザードン", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (7, "ゼニガメ", "みず", "");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (8, "カメール", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (9, "カメックス", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (10, "キャタピー", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (11, "トランセル", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (12, "バタフリー", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (13, "ビードル", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (14, "コクーン", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (15, "スピアー", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (16, "ポッポ", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (17, "ピジョン", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (18, "ピジョット", "？？？", "？？？");
+INSERT INTO pokemon (id, name, type1, type2) VALUES (19, "コラッタ", "？？？", "？？？");
--- src/main/java/com/demo/pokepb/entity/Pokemon.java
+++ src/main/java/com/demo/pokepb/entity/Pokemon.java
@@ -3,17 +3,20 @@ package com.demo.pokepb.entity;
 public class Pokemon {
     private int id;
     private String name;
+    private String type1;
+    private String type2;
 
-    public Pokemon(int id, String name) {
+    public Pokemon(int id, String name, String type1, String type2) {
         this.id = id;
         this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
     }
 
     public int getId() {
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public String getType1() { return type1; }
+    public String getType2() { return type2; }
 }
