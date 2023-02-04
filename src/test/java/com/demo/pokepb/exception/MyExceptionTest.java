8c7533eed4e879c7bd35d84ebbd99a057bd6d3b4
aoha <aoha.kurobane168@gmail.com>
2023-02-04 11:42:26 +0900

    MyExceptionのテストを追加

---
+++ src/test/java/com/demo/pokepb/exception/MyExceptionTest.java
@@ -0,0 +1,27 @@
+package com.demo.pokepb.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class MyExceptionTest {
+    @Test
+    @DisplayName("例外を投げる")
+    public void Raising_an_exception () {
+        try {
+            throw new MyException("テスト");
+        } catch(MyException e) {
+            /* MyExceptionの例外を投げることは確実だが、念の為finallyで検証させる。
+            *  そのため、catch句には何も記載しない。
+            * */
+        } finally {
+            /* 例外を投げると1回コールすることを確認 */
+            verify(mock(MyException.class), times(1));
+            /* 処理:throw new MyException()が例外クラス:MyExceptionクラスをスローすることを確認 */
+            assertThrows(MyException.class, () ->{throw new MyException("テスト");});
+        }
+    }
+}
